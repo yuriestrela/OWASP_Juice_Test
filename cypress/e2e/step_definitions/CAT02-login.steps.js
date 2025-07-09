97f1ce612193fc68c2968b550bc1512ddd8628dc
Suporte TI <suporte@CARMELCHARME.DOM>
2025-07-09 14:48:02 -0300

    correção

--- cypress/e2e/step_definitions/CAT02-login.steps.js
+++ cypress/e2e/step_definitions/CAT02-login.steps.js
@@ -1,11 +1,9 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-import { CadastroPage } from "../../support/Pages/cadastroPage"
 import { loginPage } from "../../support/Pages/loginPage";
 const Login = new loginPage()
-const Cadastro = new CadastroPage()
-const faker = require('faker-br')
-const validEmail = 'yuri@gmail.com'
-const validPassword = 'Cpftz4#Fla'
+// const faker = require('faker-br')
+// const validEmail = 'yuri@gmail.com'
+// const validPassword = 'Cpftz4#Fla'
 
 Given('que o usuário está na página de login', () => {
     Login.visit()
@@ -22,6 +20,6 @@ When('o usuário preenche os campos obrigatórios com {string}', (email, passwor
     }
 })
 
-Then('npm install --save-dev cypress @badeball/cypress-cucumber-preprocessor @bahmutov/cypress-esbuild-preprocessor', () => {
-    
+Then('o sistema deve realizar o login e ir para a página inicial', () => {
+    Login.shouldLogin()
 })
